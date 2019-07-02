05c8f0f823047b6e9cac693db08844b01493e2eb
Demiana <demiana.emil.reyad@gmail.com>
2019-07-02 12:43:24 +0300

    second commit

--- src/hello.java
+++ src/hello.java
@@ -91,6 +91,7 @@ public class hello {
                 }
                 catch(Exception ex)
                 {
+
                     JOptionPane.showMessageDialog(f,ex.getMessage());
                     System.out.println(ex.getMessage());
                 }
